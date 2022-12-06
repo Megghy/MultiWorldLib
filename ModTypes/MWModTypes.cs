9b33e59b58e96d04d6c1a3184342c0be81db950f
Megghy <megghy@qq.com>
2022-12-06 13:49:29 +0800

    update readme

--- ModTypes/MWModTypes.cs
+++ ModTypes/MWModTypes.cs
@@ -3,44 +3,44 @@ using Terraria.ModLoader;
 
 namespace MultiWorldLib.ModTypes
 {
-    //public abstract class MWModAccessorySlot<T> : ModAccessorySlot, IMWModType<T, ModAccessorySlot> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModBlockType<T> : ModBlockType, IMWModType<T, ModBlockType> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModAccessorySlot<TAttachTo> : ModAccessorySlot, IMWModType<TAttachTo, ModAccessorySlot> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModBlockType<TAttachTo> : ModBlockType, IMWModType<TAttachTo, ModBlockType> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
     //[Obsolete("Objects that need to be common between worlds cannot be modified.", true)]
-    //public abstract class MWModBossBar<T> : ModBossBar, IMWModType<T, ModBossBar> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModBossBarStyle<T> : ModBossBarStyle, IMWModType<T, ModBossBarStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModBossBar<TAttachTo> : ModBossBar, IMWModType<TAttachTo, ModBossBar> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModBossBarStyle<TAttachTo> : ModBossBarStyle, IMWModType<TAttachTo, ModBossBarStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
     //[Obsolete("Objects that need to be common between worlds cannot be modified.", true)]
-    //public abstract class MWModBuff<T> : ModBuff, IMWModType<T, ModBuff> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModModCactus<T> : ModCactus, IMWModType<T, ModCactus> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModBuff<TAttachTo> : ModBuff, IMWModType<TAttachTo, ModBuff> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModModCactus<TAttachTo> : ModCactus, IMWModType<TAttachTo, ModCactus> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
     //[Obsolete("Objects that need to be common between worlds cannot be modified.", true)]
-    //public abstract class MWModCommand<T> : ModCommand, IMWModType<T, ModCommand> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModCommand<TAttachTo> : ModCommand, IMWModType<TAttachTo, ModCommand> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
     //[Obsolete("Objects that need to be common between worlds cannot be modified.", true)]
-    //public abstract class MWModDust<T> : ModDust, IMWModType<T, ModDust> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModDust<TAttachTo> : ModDust, IMWModType<TAttachTo, ModDust> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
     //[Obsolete("Objects that need to be common between worlds cannot be modified.", true)]
-    //public abstract class MWModItem<T> : ModItem, IMWModType<T, ModItem> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModMapLayer<T> : ModMapLayer, IMWModType<T, ModMapLayer> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModMenu<T> : ModMenu, IMWModType<T, ModMenu> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModMount<T> : ModMount, IMWModType<T, ModMount> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModNPC<T> : ModNPC, IMWModType<T, ModNPC> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModPalmTree<T> : ModPalmTree, IMWModType<T, ModPalmTree> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModPrefix<T> : ModPrefix, IMWModType<T, ModPrefix> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModProjectile<T> : ModProjectile, IMWModType<T, ModProjectile> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModPylon<T> : ModPylon, IMWModType<T, ModPylon> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModTexturedType<T> : ModTexturedType, IMWModType<T, ModTexturedType> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModTile<T> : ModTile, IMWModType<T, ModTile> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModTileEntity<T> : ModTileEntity, IMWModType<T, ModTileEntity> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModWall<T> : ModWall, IMWModType<T, ModWall> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModTree<T> : ModTree, IMWModType<T, ModTree> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModSystem<T> : ModSystem, IMWModType<T, ModSystem> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModBackgroundStyle<T> : ModBackgroundStyle, IMWModType<T, ModBackgroundStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModModBiome<T> : ModBiome, IMWModType<T, ModBiome> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModGore<T> : ModGore, IMWModType<T, ModGore> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModPlayer<T> : ModPlayer, IMWModType<T, ModPlayer> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    //public abstract class MWModRarity<T> : ModRarity, IMWModType<T, ModRarity> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModSceneEffect<T> : ModSceneEffect, IMWModType<T, ModSceneEffect> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModSurfaceBackgroundStyle<T> : ModSurfaceBackgroundStyle, IMWModType<T, ModSurfaceBackgroundStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModUndergroundBackgroundStyle<T> : ModUndergroundBackgroundStyle, IMWModType<T, ModUndergroundBackgroundStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModWaterfallStyle<T> : ModWaterfallStyle, IMWModType<T, ModWaterfallStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
-    public abstract class MWModWaterStyle<T> : ModWaterStyle, IMWModType<T, ModWaterStyle> where T : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModItem<TAttachTo> : ModItem, IMWModType<TAttachTo, ModItem> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModMapLayer<TAttachTo> : ModMapLayer, IMWModType<TAttachTo, ModMapLayer> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModMenu<TAttachTo> : ModMenu, IMWModType<TAttachTo, ModMenu> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModMount<TAttachTo> : ModMount, IMWModType<TAttachTo, ModMount> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModNPC<TAttachTo> : ModNPC, IMWModType<TAttachTo, ModNPC> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModPalmTree<TAttachTo> : ModPalmTree, IMWModType<TAttachTo, ModPalmTree> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModPrefix<TAttachTo> : ModPrefix, IMWModType<TAttachTo, ModPrefix> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModProjectile<TAttachTo> : ModProjectile, IMWModType<TAttachTo, ModProjectile> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModPylon<TAttachTo> : ModPylon, IMWModType<TAttachTo, ModPylon> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModTexturedType<TAttachTo> : ModTexturedType, IMWModType<TAttachTo, ModTexturedType> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModTile<TAttachTo> : ModTile, IMWModType<TAttachTo, ModTile> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModTileEntity<TAttachTo> : ModTileEntity, IMWModType<TAttachTo, ModTileEntity> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModWall<TAttachTo> : ModWall, IMWModType<TAttachTo, ModWall> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModTree<TAttachTo> : ModTree, IMWModType<TAttachTo, ModTree> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModSystem<TAttachTo> : ModSystem, IMWModType<TAttachTo, ModSystem> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModBackgroundStyle<TAttachTo> : ModBackgroundStyle, IMWModType<TAttachTo, ModBackgroundStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModModBiome<TAttachTo> : ModBiome, IMWModType<TAttachTo, ModBiome> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModGore<TAttachTo> : ModGore, IMWModType<TAttachTo, ModGore> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModPlayer<TAttachTo> : ModPlayer, IMWModType<TAttachTo, ModPlayer> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    //public abstract class MWModRarity<TAttachTo> : ModRarity, IMWModType<TAttachTo, ModRarity> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModSceneEffect<TAttachTo> : ModSceneEffect, IMWModType<TAttachTo, ModSceneEffect> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModSurfaceBackgroundStyle<TAttachTo> : ModSurfaceBackgroundStyle, IMWModType<TAttachTo, ModSurfaceBackgroundStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModUndergroundBackgroundStyle<TAttachTo> : ModUndergroundBackgroundStyle, IMWModType<TAttachTo, ModUndergroundBackgroundStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModWaterfallStyle<TAttachTo> : ModWaterfallStyle, IMWModType<TAttachTo, ModWaterfallStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
+    public abstract class MWModWaterStyle<TAttachTo> : ModWaterStyle, IMWModType<TAttachTo, ModWaterStyle> where TAttachTo : BaseMultiWorld { public virtual string[] AlsoAttachTo { get; } }
 
     //weakAttach ↓↓↓↓↓
     public abstract class MWModSystem : ModSystem, IMWWeakModType<ModSystem> { public abstract string[] AttachTo { get; } }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MultiWorld
+
+Develping
